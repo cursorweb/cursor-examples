2330977fc7017766634c7f13eb7f0c2d76d5b9da
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2020-01-01 15:36:56 -0600

    Create index.js

---
+++ social-media-getter/index.js
@@ -0,0 +1,68 @@
+/**
+ * Who doesn't wanna get all my SM?
+ */
+
+/**Following code is not recommended to be changed.**/
+const app = require("express")();
+
+/*This code can be changed, but this is default in case the module can't load.*/
+let instagram = "https://www.instagram.com/jerrylapiz/";
+let replit = "https://repl.it/@Coder100";
+let discord = "Hithere#6537";
+let discordServer = "https://discord.gg/pSsnafT";
+let name = "Junhao Zhang";
+let github = "https://github.com/cursorweb";
+let website = "https://www.mr-lapiz.cf";
+
+var data = require("./data.js") || {
+  instagram: instagram,
+  "repl-it": replit,
+  discord: discord,
+  "discord-server": discordServer,
+  name: name,
+  github: github,
+  website: website
+};
+
+/**OFF LIMITS FROM HERE**/
+
+app.use((req, res, next) => {
+  // Set headers for each directory
+  res.append('Access-Control-Allow-Origin', '*');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append("Header-test","Header value");
+  for(i in data){
+    res.set(i, data[i])
+  }
+  res.set('Access-Control-Expose-Headers', '*')
+  next();
+})
+
+.get("/", (req, res)=>{
+  // Root. Why not get the IP address for fun?
+  var ip = req.connection.remoteAddress;
+  res.send("User with IP address "+ip.replace(/\:([\s\S]|.*)\:/gi,"")+" is currently listening.");
+});
+
+for(i in data){
+  // Get all the data for the Social Media info
+  app.get("/"+i, (req, res) => {
+    // Output everything in json
+    res.type(".json");
+    var output = {
+      ok: true,
+      output: data[i]
+    };
+    // Make it striginify
+    res.send(JSON.stringify(output));
+  });
+}
+
+app.use((req, res, next)=>{
+  // In case user has accessed something not found, we can guide them
+  res.type(".json");
+  res.send(`{"ok":false,ans:"Not found"}`)
+});
+
+// And finally, let's get the server up!
+app.listen(8080);
