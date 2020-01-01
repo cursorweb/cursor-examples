9caf4e8b63805a31119cd6a3dd4b8638560b58c6
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2020-01-01 15:56:58 -0600

    Create data.js

---
+++ social-media-getter/data.js
@@ -0,0 +1,18 @@
+module.exports = {
+  /***
+   ** Enter in some of your social media profiles!
+   ** Remember that you will have to add dashes in place of spaces:
+   ** "there-are-spaces" so that you can set the headers.
+   ** Also, to access them, go to /[name] where [name] is the object you set it to
+   ** Also, when adding dashes, make sure to put them in a quote.
+   ** 
+   ** Happy coding!
+   ***/
+    instagram: "https://www.instagram.com/jerrylapiz/",
+    "repl-it": "https://repl.it/@Coder100",
+    discord: "Hithere#6537",
+    "discord-server": "https://discord.gg/pSsnafT",
+    name: "Junhao Zhang",
+    github: "https://github.com/cursorweb",
+    website: "https://www.mr-lapiz.cf"
+};
